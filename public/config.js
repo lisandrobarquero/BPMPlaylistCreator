c05829217ed526895b407a2b72a59499941a24a3
lisandrobarquero <lizandrobs@gmail.com>
2020-07-16 22:09:05 -0600

    1.0

---
+++ public/config.js
@@ -0,0 +1,6 @@
+"use strict"
+
+//const REDIRECT_URI = 'http://localhost:8888';
+const REDIRECT_URI = 'https://bpm-playlist-creator.herokuapp.com';
+const CLIENT_ID = '3367bfd893a549c586736132c94cea43';
+const SPOTIFY_API_BASE_URL = 'https://api.spotify.com';
